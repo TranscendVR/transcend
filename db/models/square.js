e8eb809cfa9502a1280715e18be0c4ec086d8403
Joey Darbyshire <Josephmdarbyshire@gmail.com>
2017-01-25 15:55:24 -0500

    square tests complete

--- db/models/square.js
+++ db/models/square.js
@@ -4,9 +4,45 @@ const db = require('./../index');
 // square is a room or shared space (like the hallway or main room)
 // Sequelize.GEOMETRY info: http://docs.sequelizejs.com/en/latest/api/datatypes/#geometry
 const Square = db.define('square', {
-  name: Sequelize.STRING,
-  number: Sequelize.INTEGER,
-  dimensions: Sequelize.GEOMETRY
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  number: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    allowNull: false
+  },
+  // Format is [{ value: lowerBound, inclusive: true }, { value: upperBound, inclusive: false }]  Always exclude the upperBound and include the lowerBound
+  xcoord: {
+    type: Sequelize.RANGE(Sequelize.INTERGER),
+    unique: true,
+    allowNull: false,
+    set: function (coordinates) {
+      if (coordinates[0].inclusive === false) {
+        return new Error('Validation Error: Lower Bound for X is not inclusive');
+      }
+      if (coordinates[1].inclusive === true) {
+        return new Error('Validation Error: Upper Bound for X is not exclusive');
+      }
+      this.setDataValue('xcoord', coordinates);
+    }
+  },
+  zcoord: {
+    type: Sequelize.RANGE(Sequelize.INTERGER),
+    unique: true,
+    allowNull: false,
+    set: function (coordinates) {
+      if (coordinates[0].inclusive === false) {
+        return new Error('Validation Error: Lower Bound for Z is not inclusive');
+      }
+      if (coordinates[1].inclusive === true) {
+        return new Error('Validation Error: Upper Bound for Z is not exclusive');
+      }
+      this.setDataValue('zcoord', coordinates);
+    }
+  }
 });
 
 module.exports = Square;
---
+++ db/models/square.test.js
@@ -0,0 +1,72 @@
+const db = require('./../index');
+const Square = require('./square');
+const { expect } = require('chai');
+
+describe('Square', () => {
+  before('wait for the db', () => db.didSync);
+  let createdSquare;
+  beforeEach(() => {
+    createdSquare = Square.build({
+      name: 'FakeSquare',
+      number: 1,
+      xcoord: [{ value: 0, inclusive: true }, { value: 5, inclusive: false }],
+      zcoord: [{ value: 0, inclusive: true }, { value: 5, inclusive: false }]
+    });
+  });
+
+  afterEach(() => {
+    return Square.truncate({ cascade: true });
+  });
+
+  describe('Validations', () => {
+    it('creates a square with two ranges, a name, and a number value', () => {
+      createdSquare.save()
+        .then(square => {
+          expect(square.name).to.equal('FakeSquare');
+          expect(square.number).to.equal(1);
+          expect(square.xcoord.inclusive).to.eql([true, false]);
+          expect(square.zcoord.inclusive).to.eql([true, false]);
+        });
+    });
+
+    it('throws n error if the X-Coordinate Upper Bound is inclusive or the Lower Bound is exclusive', () => {
+      return createdSquare.save()
+      .then(square => {
+        return square.update({
+          xcoord: [{ value: 0, inclusive: true }, { value: 5, inclusive: true }]
+        });
+      })
+      .catch(err => {
+        expect(err.message).to.contain('Validation Error: Upper Bound for X is not exclusive');
+      })
+      .then(() => {
+        return createdSquare.update({
+          xcoord: [{ value: 0, inclusive: false }, { value: 5, inclusive: false }]
+        });
+      })
+      .catch(err => {
+        expect(err.message).to.contain('Validation Error: Lower Bound for X is not inclusive');
+      });
+    });
+
+    it('throws n error if the Z-Coordinate Upper Bound is inclusive or the Lower Bound is exclusive', () => {
+      return createdSquare.save()
+      .then(square => {
+        return square.update({
+          zcoord: [{ value: 0, inclusive: true }, { value: 5, inclusive: true }]
+        });
+      })
+      .catch(err => {
+        expect(err.message).to.contain('Validation Error: Upper Bound for Z is not exclusive');
+      })
+      .then(() => {
+        return createdSquare.update({
+          zcoord: [{ value: 0, inclusive: false }, { value: 5, inclusive: false }]
+        });
+      })
+      .catch(err => {
+        expect(err.message).to.contain('Validation Error: Lower Bound for Z is not inclusive');
+      });
+    });
+  });
+});
