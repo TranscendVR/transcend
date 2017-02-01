acf7446c7fd3e517492a338b09e9262658e51100
Sean McBride <spmcbride1201@gmail.com>
2017-02-01 17:32:57 -0500

    fix: Add class tags related to href and fix a-entity syntax
    React and A-Frame fight over the DOM and enter a race condition if I use an A-Frame tag other than a primitive.

--- browser/react/components/Beth.js
+++ browser/react/components/Beth.js
@@ -6,16 +6,16 @@ import '../../aframeComponents/aframe-hyperlink';
 export default function Beth (props) {
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <a-scene id="scene" scene-load>
+      <a-scene id="scene" class="beth" scene-load>
         <a-assets>
           <img id="background" src="/beth/background.jpg" />
         </a-assets>
         <a-entity position="-10 1 -1">
-          <a-entity geometry="primitive: sphere" rotation="0 200 0" radius="0.3" color="#000" opacity="1" roughness="0.8" href="/"></a-entity>
-          <a-entity geometry="primitive: sphere" class="highlight" visible="false" radius="0.35" rotation="0 0 0" color="#0000ff" opacity="0.6" roughness="1" metalness="0.5"></a-entity>
-          <a-entity geometry="primitive: box;" id="shadow" position="0 -1 0" color="#000" opacity="1" rotation="0 0 0" scale="0.4 1 0.4"></a-entity>
+          <a-entity geometry="primitive: sphere; radius: 0.3;" rotation="0 200 0" material="color: #000; opacity: 1; roughness: 0.8;" href="/"></a-entity>
+          <a-entity class="highlight" geometry="primitive: sphere; radius: 0.35" visible="false" rotation="0 0 0" material="color: #0000ff; opacity: 0.6; roughness: 1 metalness: 0.5"></a-entity>
+          <a-entity id="shadow" geometry="primitive: box;" material="color: #000; opacity: 1;" position="0 -1 0" rotation="0 0 0" scale="0.4 1 0.4"></a-entity>
         </a-entity>
-        <a-plane position="2 0 -4" rotation="-90 0 0" width="80" height="80" color="#444444"></a-plane>
+        <a-entity geometry="primitive: plane; width:80; height:80;" position="2 0 -4" rotation="-90 0 0" material="color:#444444;"></a-entity>
         <a-sky src="#background"></a-sky>
       </a-scene>
     </div>
