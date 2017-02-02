dd672e18389cac55fefd8c40e85b75ed1ba608f4
Sean McBride <spmcbride1201@gmail.com>
2017-02-02 13:26:38 -0500

    comment: Adding more detailed narrative for sockets

--- server/socket.js
+++ server/socket.js
@@ -1,22 +1,31 @@
+// Socket.io Logic for Node.js Server
+// Best paired with Socket.io Logic for Client
+
+// Imports and Globals
 const chalk = require('chalk');
 const { Map } = require('immutable');
-
 const store = require('./redux/store');
 const { createAndEmitUser, updateUserData, removeUserAndEmit } = require('./redux/reducers/user-reducer');
 const { getOtherUsers } = require('./utils');
-
 const channels = {};
 const sockets = {};
 
 module.exports = io => {
+  // When a user establishes a socket conenction
   io.on('connection', socket => {
     console.log(chalk.yellow(`${socket.id} has connected`));
 
-    // New user enters; create new user and new user appears for everyone else
+    // Create a user, add it to Redux, and then return the user to the Client
+    //   after they emit the sceneLoad event
     store.dispatch(createAndEmitUser(socket));
+    // ????
     socket.channels = {};
+    // Save the socket connection
     sockets[socket.id] = socket;
-
+    // After the user connects, they emit joinRoom with a string related to
+    //   the url that have joined. When the event is emitted, join them
+    //   to the appropriate room and tell the client to adversive for
+    //   WebRTC peering within that room.
     socket.on('joinRoom', (winLoc) => {
       const myRoom = winLoc.pathname;
       console.log(`Appending to socket? ${myRoom}`);
@@ -25,7 +34,12 @@ module.exports = io => {
       socket.emit('initWebRTC');
     });
 
-    // This will send all of the current users to the user that just connected
+    // After the client has generated a user, created an avatar, placed it on
+    //   the DOM, and given in first-person A-Frame components, the client emits
+    //   the getOthers event to perform the initial render of the other avatars
+    //   in the same room. Currently this generates an immutable map of all users
+    //   other than the client's own avatar and forces the client to perform local
+    //   client-side filtering.
     // TODO: Only return the list of people in the current scene
     socket.on('getOthers', () => {
       const allUsers = store.getState().users;
