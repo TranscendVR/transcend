05e2aaa95a35e644b782766faa194a4fb31f1b95
Sean McBride <spmcbride1201@gmail.com>
2017-02-02 23:43:52 -0500

    feat: More grockable WebRTC logic
    Only one room at a time.
    Logical Rooms
    Acutal use of socket.io rooms

--- browser/webRTC/client.js
+++ browser/webRTC/client.js
@@ -1,9 +1,6 @@
 // WebRTC Logic for Client
 
-// Config Variables
-const USE_AUDIO = true;
-const USE_VIDEO = false;
-const DEFAULT_CHANNEL = 'some-global-channel-name';
+
 /** You should probably use a different stun server doing commercial stuff **/
 /** Also see: https://gist.github.com/zziuni/3741933 **/
 const ICE_SERVERS = [
@@ -16,67 +13,80 @@ let localMediaStream = null; // Our microphone
 let peers = {}; // keep track of our peer connections, indexed by peer_id (aka socket.io id)
 let peerMediaElements = {};  // keep track of our <audio> tags, indexed by peer_id
 
-export function setupLocalMedia (callback, errorback) {
+// setupLocalMedia retrieves the socket.io object to use for WebRTC signaling.
+//   If the microphone audio stream is already active, the client immediately
+//   invokes 'joinChatRoom' to join the room DEFAULT_ROOM and exits the function.
+//   If the microphone audio stream is not yet active, the client checks for the correct
+//   vendor prefix for getUserMedia and then invokes getUserMedia to obtain an audio stream.
+//   Once the stream is active, it's saved to the external variable localMediaStream and
+//   set as the srcObject of the AUDIO tag of ID localAudio. The AUDIO tag is muted to keep
+//   the user from getting headaches hearing themselves. Finally, the client invokes
+//   'joinChatRoom' to join the room DEFAULT_ROOM.
+// If the user decides not to share their microphone, they are presented with an error
+//   informing them that voice is unavailable.
+//
+// TODO: Use an actual meaningful room name.
+
+// joinChatRoom emits the 'join' event with the socket.io room that the
+//   user ID associated with the socket should join and then user for
+//   WebRTC signalling.
+
+export function joinChatRoom (room, errorback) {
+  if (!room) {
+    console.log('No room was provided');
+    return;
+  }
   if (signalingSocket === null) {
     signalingSocket = window.socket;
   }
   if (localMediaStream != null) {  /* ie, if we've already been initialized */
-    joinChatChannel(DEFAULT_CHANNEL, { 'whatever-you-want-here': 'stuff' });
+    signalingSocket.emit('joinChatRoom', room);
     return;
   }
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  /* Ask user for permission to use the computers microphone and/or camera,
-  * attach it to an <audio> or <video> tag if they give us access. */
   console.log('Requesting access to local audio / video inputs');
-  navigator.getUserMedia({ 'audio': USE_AUDIO, 'video': USE_VIDEO }, function (stream) {
-    console.log('Access granted to audio/video');
+  navigator.getUserMedia({ 'audio': true, 'video': false }, function (stream) {
+    console.log('Access granted to audio');
     localMediaStream = stream;
     const audioEl = document.getElementById('localAudio');
     audioEl.muted = true;
     audioEl.srcObject = stream;
-    joinChatChannel(DEFAULT_CHANNEL, { 'whatever-you-want-here': 'stuff' });
+    signalingSocket.emit('joinChatRoom', room);
   },
-  function () { /* user denied access to a/v */
-    console.log('Access denied for audio/video');
-    window.alert('You chose not to provide access to the camera/microphone, demo will not work.');
-    if (errorback) errorback();
-  });
+    function () { /* user denied access to a/v */
+      console.log('Access denied for audio/video');
+      window.alert('You chose not to provide access to your microphone, so real-time voice chat is unavailable.');
+      if (errorback) errorback();
+    });
 }
 
-export function disconnectUser () {
-  for (const peerId in peerMediaElements) {
-    peerMediaElements[peerId].remove();
-  }
-  for (const peerId in peers) {
-    peers[peerId].close();
-  }
-  peers = {};
-  peerMediaElements = {};
-}
 
-function joinChatChannel (channel, userdata) {
-  signalingSocket.emit('join', { 'channel': channel, 'userdata': userdata });
-}
+
 // Don't need this yet, but might in the future
-// function partChatChannel (channel) {
-//   signalingSocket.emit('part', channel);
-// }
+export function leaveChatRoom (room) {
+  signalingSocket.emit('leaveChatRoom', room);
+}
 
+// accepts conifg
 export function addPeerConn (config) {
   console.log('Signaling server said to add peer:', config);
   const peerId = config.peer_id;
+  // If for some reason, this client aready is connected to the peer, return
   if (peerId in peers) {
-    /* This could happen if the user joins multiple channels where the other peer is also in. */
     console.log('Already connected to peer ', peerId);
     return;
   }
+
+  // Create a webRTC peer connection to the ICE servers
   const peerConnection = new webkitRTCPeerConnection(
     { 'iceServers': ICE_SERVERS },
-    { 'optional': [{ 'DtlsSrtpKeyAgreement': true }] } /* this will no longer be needed by chrome
+    { 'optional': [{ 'DtlsSrtpKeyAgreement': true }] }
+    /* this will no longer be needed by chrome
     * eventually (supposedly), but is necessary
     * for now to get firefox to talk to chrome */
   );
   peers[peerId] = peerConnection;
+
   // I'm not 100% sure what this does, but it sets up ice candidates ¯\_(ツ)_/¯
   peerConnection.onicecandidate = function (event) {
     if (event.candidate) {
@@ -90,6 +100,9 @@ export function addPeerConn (config) {
     }
   };
 
+  // When we recieve a remote audio stream from the peer, create an audio tag and append it to the
+  //   body tag of the DOM. Set the audio tag with an ID equal to the peerID, set it to autoplay,
+  //   and save the audio stream to the peerMediaElements object
   peerConnection.onaddstream = function (event) {
     console.log('onAddStream', event);
     const remoteAudio = document.createElement('audio');
@@ -115,7 +128,7 @@ export function addPeerConn (config) {
         peerConnection.setLocalDescription(localDescription,
           function () {
             signalingSocket.emit('relaySessionDescription',
-            { 'peer_id': peerId, 'session_description': localDescription });
+              { 'peer_id': peerId, 'session_description': localDescription });
             console.log('Offer setLocalDescription succeeded');
           },
           function () { window.alert('Offer setLocalDescription failed!'); }
@@ -159,7 +172,7 @@ export function setRemoteAnswer (config) {
             peer.setLocalDescription(localDescription,
               function () {
                 signalingSocket.emit('relaySessionDescription',
-                { 'peer_id': peerId, 'session_description': localDescription });
+                  { 'peer_id': peerId, 'session_description': localDescription });
                 console.log('Answer setLocalDescription succeeded');
               },
               function () { window.alert('Answer setLocalDescription failed!'); }
@@ -183,4 +196,15 @@ export function setIceCandidate (config) {
   const peer = peers[config.peer_id];
   const iceCandidate = config.ice_candidate;
   peer.addIceCandidate(new RTCIceCandidate(iceCandidate));
+}
+
+export function disconnectUser () {
+  for (const peerId in peerMediaElements) {
+    peerMediaElements[peerId].remove();
+  }
+  for (const peerId in peers) {
+    peers[peerId].close();
+  }
+  peers = {};
+  peerMediaElements = {};
 }
--- server/socket.js
+++ server/socket.js
@@ -7,7 +7,7 @@ const { Map } = require('immutable');
 const store = require('./redux/store');
 const { createAndEmitUser, updateUserData, removeUserAndEmit } = require('./redux/reducers/user-reducer');
 const { getOtherUsers } = require('./utils');
-const channels = {};
+const rooms = {};
 const sockets = {};
 
 module.exports = io => {
@@ -18,21 +18,8 @@ module.exports = io => {
     // Create a user, add it to Redux, and then return the user to the Client
     //   after they emit the sceneLoad event
     store.dispatch(createAndEmitUser(socket));
-    // ????
-    socket.channels = {};
     // Save the socket connection
     sockets[socket.id] = socket;
-    // After the user connects, they emit joinRoom with a string related to
-    //   the url that have joined. When the event is emitted, join them
-    //   to the appropriate room and tell the client to adversive for
-    //   WebRTC peering within that room.
-    socket.on('joinRoom', (winLoc) => {
-      const myRoom = winLoc.pathname;
-      console.log(`Appending to socket? ${myRoom}`);
-      socket.join(myRoom);
-      console.log(`${socket.id} has joined the room ${myRoom}`);
-      socket.emit('initWebRTC');
-    });
 
     // After the client has generated a user, created an avatar, placed it on
     //   the DOM, and given in first-person A-Frame components, the client emits
@@ -81,63 +68,55 @@ module.exports = io => {
 
     // When a socket disconnects, invoke the 'removeUserAndEmit' thunk, which removes the user
     //   associated with the socket from the redux store and broadcast the 'removeUser' to all
-    //   clients. Then do some black-magic to remove the user from socket.channels, which has
+    //   clients. Then do some black-magic to remove the user from socket.rooms, which has
     //   something to do with WebRTC (perhaps signalling???) Then delete the disconnected socket
-    //   from the sockets array.
-    // TODO: Perhaps the broadcast i
+    //   from the sockets object.
+    // TODO: Perhaps the broadcast only needs to be emitted to a specific room
     socket.on('disconnect', () => {
       store.dispatch(removeUserAndEmit(socket));
       console.log(chalk.magenta(`${socket.id} has disconnected`));
-      for (const channel in socket.channels) {
-        part(channel);
-      }
+      leaveChatRoom(socket.currentChatRoom);
       console.log(`[${socket.id}] disconnected`);
       delete sockets[socket.id];
     });
 
-    // Connects a new user to a channel and emits to all other users to initiate a new RTCPeerConnection with them
-    socket.on('join', function (config) {
-      console.log(`[${socket.id}] join ${config}`);
-      const channel = config.channel;
-      // const userdata = config.userdata;
-
-      if (channel in socket.channels) {
-        console.log(`[${socket.id}] ERROR: already joined ${channel}`);
-        return;
+    // Connects a new user to a room and emits to all other users to initiate a new RTCPeerConnection with them
+    // A client emits the joinChatRoom event to the server immediately after establishing a conenction
+    //   and establishing a local audio feed. The even passes the window.location.pathname attribute
+    //   to relate chat rooms to A-Scenes. After making sure that a user isn't somehow already in the
+    //   chat room, the chat room is added as an object literal to the rooms object with a key equal
+    //   to the room parameter. The rooms object represents the complete state of all chat rooms in the
+    //   app and all users within each chat room. If there are other users located in the chat room,
+    //   the new user and all users already in the room are sent the 'addPeer' event. The new user is
+    //   told to create an offer, which establishes the peer-to-peer connection. Finally, the room is
+    //   set as the user's room in socket.currentChatRoom and the user is added to the roster of users in
+    //   the chat room in the rooms object.
+    socket.on('joinChatRoom', function (room) {
+      console.log(`[${socket.id}] join ${room}`);
+      if (!(room in rooms)) {
+        rooms[room] = {};
       }
-
-      if (!(channel in channels)) {
-        channels[channel] = {};
-      }
-
-      for (const id in channels[channel]) {
-        channels[channel][id].emit('addPeer', { 'peer_id': socket.id, 'should_create_offer': false });
+      for (const id in rooms[room]) {
+        rooms[room][id].emit('addPeer', { 'peer_id': socket.id, 'should_create_offer': false });
         socket.emit('addPeer', { 'peer_id': id, 'should_create_offer': true });
       }
-
-      channels[channel][socket.id] = socket;
-      socket.channels[channel] = channel;
+      rooms[room][socket.id] = socket;
+      socket.join(room);
+      socket.currentChatRoom = room;
     });
 
-    // Removes a user from a channel and tells all other users to discontinue their connection with them
-    function part (channel) {
-      console.log(`[${socket.id}] part`);
-
-      if (!(channel in socket.channels)) {
-        console.log(`[${socket.id}] ERROR: not in ${channel}`);
-        return;
-      }
-
-      delete socket.channels[channel];
-      delete channels[channel][socket.id];
-
-      for (const id in channels[channel]) {
-        channels[channel][id].emit('removePeer', { 'peer_id': socket.id });
+    // leaveChatRoom clears the user from the rooms object and wipes the user's socket.currentChatRoom.
+    function leaveChatRoom () {
+      const room = socket.currentChatRoom;
+      console.log(`[${socket.id}] leaveChatRoom`);
+      socket.leave(room);
+      delete rooms[room][socket.id];
+      for (const id in rooms[room]) {
+        rooms[room][id].emit('removePeer', { 'peer_id': socket.id });
         socket.emit('removePeer', { 'peer_id': id });
       }
     }
-
-    socket.on('part', part);
+    socket.on('leaveChatRoom', (room) => leaveChatRoom(room));
 
     // If any user is an Ice Candidate, tells other users to set up a ICE connection with them
     socket.on('relayICECandidate', function (config) {
