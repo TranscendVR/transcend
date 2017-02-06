76126df5ea10690f131c8208e8e5fdd152e18141
Yoo-Nah Park <ypark2270@gmail.com>
2017-02-06 16:16:30 -0500

    add smaller class photo; add timeout in assets

--- browser/react/components/AssetLoader.js
+++ browser/react/components/AssetLoader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AssetLoader (props) {
   return (
-    <a-assets>
+    <a-assets timeout="10000">
       <img id="yoonah-background" src="/yoonah/background.jpg" />
       <img id="beth-background" src="/beth/background.jpg" />
       <img id="joey-background" src="/joey/background.jpg" />
@@ -14,7 +14,7 @@ export default function AssetLoader (props) {
       <a-asset-item id="punch-reader" src="/sean/card-reader.dae"></a-asset-item>
 
       {/* Lobby assets */}
-      <img id="slide" src="/img/class.png"/>
+      <img id="slide" src="/img/class_copy.png"/>
       <img id="podium" src="/img/fullstack.png"/>
       <img id="floorText" src="/img/hardwood.jpg"/>
       <a-mixin id="chair-part" geometry="primitive: box" material="color: brown"></a-mixin>
