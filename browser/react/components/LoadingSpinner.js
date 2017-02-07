4654bc52f441bc3d9dca11bf22bde9939a3e7e94
Sean McBride <spmcbride1201@gmail.com>
2017-02-07 18:40:15 -0500

    fix: Double quotes for color property.

--- browser/react/components/LoadingSpinner.js
+++ browser/react/components/LoadingSpinner.js
@@ -4,7 +4,7 @@ import CircularProgress from 'material-ui/CircularProgress';
 export default function LoadingSpinner (props) {
   return (
     <div style={{ width: '100%', height: '100%', backgroundColor: '#7AC9F1', backgroundImage: 'url(/images/background.png)', display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }}>
-      <CircularProgress size={200} thickness={40} color='#286CB5'/>
+      <CircularProgress size={200} thickness={40} color="#286CB5"/>
     </div>
   );
 }
