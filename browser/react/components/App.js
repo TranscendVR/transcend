cced2f12e4d3efaee909d187a739a6393ecbf10a
Beth Qiang <bethany.qiang@gmail.com>
2017-02-08 20:48:24 -0600

    Add random spheres
    - Things are mostly back to the way they were
    - This rotation thing is a problem again

--- browser/react/components/App.js
+++ browser/react/components/App.js
@@ -24,6 +24,14 @@ function App (props) {
         {/*<AssetLoader />
         {props.children}*/}
 
+        {/*random shapes for positioning*/}
+        <a-entity geometry="primitive: box" material="color: orange; shader: flat" position="0 1 -4"></a-entity>
+        <a-entity geometry="primitive: sphere; radius: 2.2" material="color: #F16745" position="0 2 -10"></a-entity>
+        <a-entity geometry="primitive: sphere; radius: 1.75" material="color: #7BC8A4" position="-5 1.75 -5"></a-entity>
+        <a-entity geometry="primitive: sphere; radius: 1" material="color: #4CC3D9" position="5 1 0"></a-entity>
+        <a-entity geometry="primitive: sphere; radius: 2.5" material="color: #FFC65D" position="-7.5 0.5 0"></a-entity>
+        <a-entity geometry="primitive: sphere; radius: 5" material="color: #93648D" position="10 0 -6"></a-entity>
+
         <a-entity id="ground" class="selectable" geometry="primitive: box; width: 100; height: 100; depth: 100" material="color: green" position="0 -50 0"></a-entity>
         <a-sky color="blue"></a-sky>
       </a-scene>
