0f304bbc22eb6a98963118999a8c57729c1e998e
Beth Qiang <bethany.qiang@gmail.com>
2017-02-10 08:45:48 -0600

    Curious what's going to happen...

--- browser/react/index.js
+++ browser/react/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 import { Router, Route, browserHistory, IndexRoute, IndexRedirect } from 'react-router';
 import App from './components/App';
-import Sean from './components/Sean';
+// import Sean from './components/Sean';
 import Beth from './components/Beth';
 import Yoonah from './components/Yoonah';
 import Joey from './components/Joey';
@@ -52,7 +52,7 @@ ReactDOM.render(
           <Route path="/vr" component={App} onEnter={confirmLogin}>
             <IndexRedirect to="lobby" />
             <Route path="lobby" component={Lobby} />
-            <Route path="sean" component={Sean} />
+            {/*<Route path="sean" component={Sean} />*/}
             <Route path="beth" component={Beth} />
             <Route path="yoonah" component={Yoonah} />
             <Route path="joey" component={Joey} />
