0e0aa649718c8c911eb5d4bc2908760ca4a9b2b0
Sean McBride <spmcbride1201@gmail.com>
2017-02-11 13:17:12 -0500

    fix: Restore dumbness of LoadingSpinner.

--- browser/react/components/LoadingSpinner.js
+++ browser/react/components/LoadingSpinner.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
-// const style = { width: '100%', height: '100%', backgroundColor: '#7AC9F1', backgroundImage: 'url(/images/background.png)', display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'center' };
 const style = { width: '100%', height: '100%', display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'center' };
 
-export default class LoadingSpinner extends React.Component {
-
-  render () {
-    return (
-      <div style={style}>
-        <CircularProgress size={200} thickness={40} color="#286CB5" />
-      </div>
-    );
-  }
+export default function LoadingSpinner (props) {
+  return (
+    <div style={style}>
+      <CircularProgress size={200} thickness={40} color="#286CB5" />
+    </div>
+  );
 }
