d8a32a27848a95072230ed1a8bc2d7f6fa8cd3f5
Beth Qiang <bethany.qiang@gmail.com>
2017-02-12 16:27:15 -0600

    Make ray smaller & add Daydream controller model

--- browser/add-controller-and-listeners.js
+++ browser/add-controller-and-listeners.js
@@ -12,19 +12,25 @@ export default function addControllerAndListeners (userId) {
 
   const avatar = document.getElementById(userId);
 
+  const intermediary = document.createElement('a-entity');
+  avatar.appendChild(intermediary);
+  intermediary.setAttribute('tracked-controls', 'id:Daydream Controller');
+
   // Put remote on DOM
   const remote = document.createElement('a-entity');
-  avatar.appendChild(remote);
+  intermediary.appendChild(remote);
   remote.setAttribute('id', 'remote');
-  remote.setAttribute('daydream-controller', true);
+  // remote.setAttribute('daydream-controller', true);
+  remote.setAttribute('position', '0 -0.5 -0.5');
+  remote.setAttribute('obj-model', 'obj:https://raw.githubusercontent.com/TechnoBuddhist/VR-Controller-Daydream/master/vr_controller_daydream.obj; mtl:https://raw.githubusercontent.com/TechnoBuddhist/VR-Controller-Daydream/master/vr_controller_daydream.mtl');
 
   // Put ray on DOM
   const ray = document.createElement('a-entity');
   remote.appendChild(ray);
   ray.setAttribute('id', 'ray');
-  ray.setAttribute('geometry', 'buffer: false; primitive: cone; radius-bottom: 0.005; radius-top: 0.001; height: 8');
+  ray.setAttribute('geometry', 'buffer: false; primitive: cylinder; radius: 0.003; height: 6');
   ray.setAttribute('material', 'color: cyan');
-  ray.setAttribute('position', '0 0 -4');
+  ray.setAttribute('position', '0 0 -3');
   ray.setAttribute('rotation', '-90 0 0');
 
   // Put position guide on DOM
@@ -33,7 +39,7 @@ export default function addControllerAndListeners (userId) {
   remote.appendChild(positionGuide);
   positionGuide.setAttribute('geometry', 'primitive: sphere; radius: 0.05');
   positionGuide.setAttribute('material', 'color: white');
-  positionGuide.setAttribute('position', '0 0 -8');
+  positionGuide.setAttribute('position', '0 0 -6');
 
   // Attach collider component to the avatar
   // The collider component primarily determines the shooting of rays from the controller to the ground
