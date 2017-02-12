9fa83928ffc09f9a5ebbf953823722ba7ac06cc8
Sean McBride <spmcbride1201@gmail.com>
2017-02-11 20:54:08 -0500

    Feat: wrapped skins in cylinder to allow click events.

--- browser/react/components/ChangingRoom.js
+++ browser/react/components/ChangingRoom.js
@@ -25,22 +25,22 @@ export default class Sean extends React.Component {
           floorTexture="#floorText"
           ceilingColor="#998403" />
 
-        <Mannequin x="-14" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="3djesus" nickname="3D Jesus" />
-        <Mannequin x="-12" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="agentsmith" nickname="Agent Smith" />
-        <Mannequin x="-10" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="batman" nickname="Batman" />
-        <Mannequin x="-8" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="char" nickname="Minecraft" />
-        <Mannequin x="-6" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="god" nickname="God" />
-        <Mannequin x="-4" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Iron-Man-Minecraft-Skin" nickname="Iron Man" />
-        <Mannequin x="-2" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="jetienne" nickname="Jetienne" />
-        <Mannequin x="0" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Joker" nickname="Joker" />
-        <Mannequin x="2" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Mario" nickname="Mario" />
-        <Mannequin x="4" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="martialartist" nickname="Martial Artist" />
-        <Mannequin x="6" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="robocop" nickname="Robocop" />
-        <Mannequin x="8" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Sonicthehedgehog" nickname="Sonic" />
-        <Mannequin x="10" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Spiderman" nickname="Spiderman" />
-        <Mannequin x="12" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="Superman" nickname="Superman" />
-        <Mannequin x="14" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="theflash" nickname="theflash" />
-        <Mannequin x="16" y="1.25" z="10" xrot="0" yrot="0" zrot="0" skin="woody" nickname="woody" />
+        <Mannequin x="-14" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="3djesus" nickname="3D Jesus"/>
+        <Mannequin x="-12" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="agentsmith" nickname="Agent Smith" />
+        <Mannequin x="-10" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="batman" nickname="Batman" />
+        <Mannequin x="-8" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="char" nickname="Minecraft" />
+        <Mannequin x="-6" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="god" nickname="God" />
+        <Mannequin x="-4" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Iron-Man-Minecraft-Skin" nickname="Iron Man" />
+        <Mannequin x="-2" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="jetienne" nickname="Jetienne" />
+        <Mannequin x="0" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Joker" nickname="Joker" />
+        <Mannequin x="2" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Mario" nickname="Mario" />
+        <Mannequin x="4" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="martialartist" nickname="Martial Artist" />
+        <Mannequin x="6" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="robocop" nickname="Robocop" />
+        <Mannequin x="8" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Sonicthehedgehog" nickname="Sonic" />
+        <Mannequin x="10" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Spiderman" nickname="Spiderman" />
+        <Mannequin x="12" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Superman" nickname="Superman" />
+        <Mannequin x="14" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="theflash" nickname="theflash" />
+        <Mannequin x="16" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="woody" nickname="woody" />
 
         <Teleporter
           color="green"
