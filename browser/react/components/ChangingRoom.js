ba2b37cf2a9248e6bb6cdede0bdddd3633af72a6
Sean McBride <spmcbride1201@gmail.com>
2017-02-11 23:14:19 -0500

    feat: Added a bunch of female skins.

--- browser/react/components/ChangingRoom.js
+++ browser/react/components/ChangingRoom.js
@@ -40,8 +40,15 @@ export default class Sean extends React.Component {
         <Mannequin x="10" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Spiderman" nickname="Spiderman" />
         <Mannequin x="12" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="Superman" nickname="Superman" />
         <Mannequin x="14" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="theflash" nickname="theflash" />
-        <Mannequin x="16" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="woody" nickname="woody" />
-        <Mannequin x="18" y="0.75" z="10" xrot="0" yrot="0" zrot="0" skin="powerRanger" nickname="Power Ranger" />
+        <Mannequin x="-14" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="woody" nickname="woody" />
+        <Mannequin x="-12" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="powerRanger" nickname="Power Ranger" />
+        <Mannequin x="-10" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="catwoman" nickname="Catwoman" />
+        <Mannequin x="-8" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="blackWidow" nickname="Black Widow" />
+        <Mannequin x="-6" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="evilQueen" nickname="Evil Queen" />
+        <Mannequin x="-4" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="graceHopper" nickname="Grace Hopper" />
+        <Mannequin x="-2" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="princessBelle" nickname="Princess Belle" />
+        <Mannequin x="0" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="skaterGirl" nickname="Skater Girl" />
+        <Mannequin x="2" y="0.75" z="-10" xrot="0" yrot="180" zrot="0" skin="catnissEverdeen" nickname="Catniss Everdeen" />
 
         <Teleporter
           color="green"
