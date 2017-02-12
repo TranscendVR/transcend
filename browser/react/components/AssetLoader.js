6ee59156cc24d68acf59d7ee4f085f4c2a10210c
Yoo-Nah Park <ypark2270@gmail.com>
2017-02-11 22:02:26 -0500

    merge master

--- browser/react/components/AssetLoader.js
+++ browser/react/components/AssetLoader.js
@@ -3,12 +3,6 @@ import React from 'react';
 export default function AssetLoader (props) {
   return (
     <a-assets timeout="60000" >
-      {/* Sean's Room assets */}
-      <a-asset-item id="IBM-360" src="/sean/console.dae"></a-asset-item>
-      <a-asset-item id="terminal-typewriter" src="/sean/teletype.dae"></a-asset-item>
-      <a-asset-item id="tape-drive" src="/sean/tape-reader.dae"></a-asset-item>
-      <a-asset-item id="disk-pack" src="/sean/diskpack.dae"></a-asset-item>
-      <a-asset-item id="punch-reader" src="/sean/card-reader.dae"></a-asset-item>
 
       {/* Lobby assets */}
       <img id="slide" src="/img/class_copy.png"/>
